67685b9f4cda4c68db699b372e79769f639944c3
“Avishek <avishekroy87@gmail.com>
2025-06-07 20:43:26 +0530

    firstcommit1

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -4,13 +4,22 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBQsJ5MS4-MacHnO5wxQX8V2tGe0y6eCXo",
+//   authDomain: "myecommerce2-7b8fc.firebaseapp.com",
+//   projectId: "myecommerce2-7b8fc",
+//   storageBucket: "myecommerce2-7b8fc.appspot.com",
+//   messagingSenderId: "715233167432",
+//   appId: "1:715233167432:web:3a0acec83b123399d3307d"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBQsJ5MS4-MacHnO5wxQX8V2tGe0y6eCXo",
-  authDomain: "myecommerce2-7b8fc.firebaseapp.com",
-  projectId: "myecommerce2-7b8fc",
-  storageBucket: "myecommerce2-7b8fc.appspot.com",
-  messagingSenderId: "715233167432",
-  appId: "1:715233167432:web:3a0acec83b123399d3307d"
+  apiKey: "AIzaSyCW-L7vq2xQrB7pieSo6f6S2ep1OAvOU6Y",
+  authDomain: "consumerapp-6f284.firebaseapp.com",
+  projectId: "consumerapp-6f284",
+  storageBucket: "consumerapp-6f284.firebasestorage.app",
+  messagingSenderId: "54211313575",
+  appId: "1:54211313575:web:c0be0dda26ca60e5124082"
 };
 
 // Initialize Firebase
